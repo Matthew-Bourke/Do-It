0a19122c4379471c11d5e5002eb74938387db23f
Matthew Bourke <Matt@Matthews-MacBook-Pro.local>
2017-08-19 14:07:33 +1000

    Added tableview, specified class 'Task' and added 3 tasks to list.

---
+++ Do It/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Do It
+//
+//  Created by Matthew Bourke on 19/8/17.
+//  Copyright © 2017 Matthew Bourke. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- Do It/ViewController.swift
+++ Do It/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var taskList: UITableView!
+    
+    var tasks : [Task] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tasks = makeTasks()
+        
+        taskList.dataSource = self
+        taskList.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Do uni work"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Buy cheese"
+        task3.important = false
+        
+        return [task1, task2, task3]
     }
 
     override func didReceiveMemoryWarning() {
